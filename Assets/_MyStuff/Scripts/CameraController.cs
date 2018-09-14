72f40eea2fc82e6aa948ccf2880d64beaf4b2806
Shubham Tyagi <styagi.2301@gmail.com>
2018-09-15 01:00:34 +0530

    Added Camera Controller script
    Camera controller has been added that takes control of :
    1- Moving the camera (following the tanks)
    2- Zooming in and out  based on the distance between the tanks

---
+++ Assets/_MyStuff/Scripts/CameraController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float dampValue = 0.2f; // time to move & zoom the camera to desired position (for smoothness in smoothdamp)
+    public float minOrthographicSize = 4f; // Max Zoom in Distance
+    public float screenEdgeBuffer = 4f; // amount added to the camera size for edge offset
+    public Transform[] targets;
+
+    private new Camera camera;
+    private float refZoomSpeed; // required a ref in smooth damp
+    private Vector3 refMoveVelocity; // required a ref in smooth damp
+    private Vector3 desiredPosition; // based on CameraController position and not on actual camera position
+
+    void Start()
+    {
+        camera = GetComponentInChildren<Camera>();
+    }
+
+    void FixedUpdate()
+    {
+        // Moving and Zooming in FixedUpdate because of our 
+        // tanks movements are also calculated in FixedUpdate
+        // it will keep the sync between our tanks and camera
+        Move();
+        Zoom();
+    }
+
+    private void Move()
+    {
+        // Finding a center point of all active tanks in the scene
+        FindAveragePosition();
+
+        transform.position = Vector3.SmoothDamp(transform.position, desiredPosition, ref refMoveVelocity, dampValue);
+    }
+    private void FindAveragePosition()
+    {
+        Vector3 _avgPos = Vector3.zero;
+        int _numberOfTanksFound = 0;
+
+        for (int i = 0; i < targets.Length; i++)
+        {
+            if (!(targets[i].gameObject.activeSelf)) // skipping any inactive tank
+                continue;
+
+            _avgPos += targets[i].position;
+            _numberOfTanksFound++;
+        }
+
+        if (_numberOfTanksFound > 0)
+            _avgPos /= _numberOfTanksFound;
+
+        _avgPos.y = transform.position.y; // dont want to change the y value
+
+        desiredPosition = _avgPos;
+    }
+
+    private void Zoom()
+    {
+        // end point distance in y-axis from the center is orthographic size of the camea;
+        // end point distance in x-axis from the center is orthographic size * aspect;
+        // aspect = width /  height (eg: for resolution 1920:1080 ---> aspect will be 1.77);
+
+        // finding orthographic size by the formula size = distance / aspect
+        // here distance is the max distance of a tank from the center in either axis
+        float requiredSize = FindRequiredOrthographicSize();
+
+        camera.orthographicSize = Mathf.SmoothDamp(camera.orthographicSize, requiredSize, ref refZoomSpeed, dampValue);
+    }
+    private float FindRequiredOrthographicSize()
+    {
+        float _requiredSize = 0f;
+
+        // we need to calculate zoom size based on the desired position(camera controller position) and not on camera's position.
+        // we need to convert desired position from world space to local space.
+        // this conversion is required to get the local space as 
+        // world space starts from bottom right and local space starts from center
+        // and the formala to calculate the size requires distance from the center
+        // we also need to do this conversion for each tank(target).
+        Vector3 _desiredPosInLocalSpace = transform.InverseTransformPoint(desiredPosition);
+
+        for (int i = 0; i < targets.Length; i++)
+        {
+            if (!(targets[i].gameObject.activeSelf))
+                continue;
+
+            Vector3 _targetPosInLocalSpace = transform.InverseTransformPoint(targets[i].position);
+
+            Vector3 _distanceToTank = _targetPosInLocalSpace - _desiredPosInLocalSpace;
+
+            // we need to find the max size between x and y
+            _requiredSize = Mathf.Max(_requiredSize, Mathf.Abs(_distanceToTank.y));
+
+            // for x-axis we need to / it by the aspect to find the size acc. to the formula
+            _requiredSize = Mathf.Max(_requiredSize, Mathf.Abs(_distanceToTank.x) / camera.aspect);
+        }
+
+        _requiredSize += screenEdgeBuffer; // to maintain the edge offset
+
+        // if the max of x and y is still less than minOrthographicSize
+        // then set size to minOrthographicSize.
+        if (_requiredSize < minOrthographicSize)
+            _requiredSize = minOrthographicSize;
+
+        return _requiredSize;
+    }
+
+    public void SetStartPositionAndSize()
+    {
+        FindAveragePosition();
+        transform.position = desiredPosition;
+
+        camera.orthographicSize = FindRequiredOrthographicSize();
+    }
+}
---
+++ Assets/_MyStuff/Scripts/Tank.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tank : MonoBehaviour
+{
+    public int playerNumber = 1; // Assigning a ID to each player
+    public float moveSpeed = 10f;
+    public float turnSpeed = 180f;
+    public AudioSource movementAudio;
+    public AudioClip engineDriving;
+    public AudioClip engineIdle;
+    public float audioPitchRange = 0.2f;
+
+    private string moveAxisName;
+    private string turnAxisName;
+    private Rigidbody rb;
+    private float moveInputValue;
+    private float turnInputValue;
+    private float originalPitch;
+
+    private void Awake()
+    {
+        if (GetComponent<Rigidbody>())
+            rb = GetComponent<Rigidbody>();
+        else
+            Debug.LogError("RigidBody is missing on the Tank");
+    }
+
+    private void OnEnable()
+    {
+        rb.isKinematic = false;
+        moveInputValue = 0f;
+        turnInputValue = 0f;
+    }
+
+    private void OnDisable()
+    {
+        rb.isKinematic = true;
+    }
+
+    void Start()
+    {
+        moveAxisName = "Vertical" + playerNumber;
+        turnAxisName = "Horizontal" + playerNumber;
+        originalPitch = movementAudio.pitch;
+    }
+
+    void Update()
+    {
+        moveInputValue = Input.GetAxis(moveAxisName);
+        turnInputValue = Input.GetAxis(turnAxisName);
+        EngineAudio();
+    }
+
+    private void EngineAudio()
+    {
+        // Adjusting audio according to the tank state (Driving or Ideling)
+        if (Mathf.Abs(moveInputValue) < 0.1f && Mathf.Abs(turnInputValue) < 0.1f)
+        {
+            if (movementAudio.clip == engineDriving)
+            {
+                movementAudio.clip = engineIdle;
+                movementAudio.pitch = Random.Range(originalPitch - audioPitchRange, originalPitch + audioPitchRange);
+                movementAudio.Play();
+            }
+        }
+        else
+        {
+            if (movementAudio.clip == engineIdle)
+            {
+                movementAudio.clip = engineDriving;
+                movementAudio.pitch = Random.Range(originalPitch - audioPitchRange, originalPitch + audioPitchRange);
+                movementAudio.Play();
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (rb != null)
+        {
+            Move();
+            Turn();
+        }
+    }
+
+    private void Move()
+    {
+        // Adjusting position of tank
+        Vector3 movement = transform.forward * moveInputValue * moveSpeed * Time.deltaTime;
+        rb.MovePosition(rb.position + movement);
+
+
+    }
+
+    private void Turn()
+    {
+        // Adjusting rotation of tank
+        float turn = turnInputValue * turnSpeed * Time.deltaTime;
+        Quaternion rotation = Quaternion.Euler(0f, turn, 0f);
+        rb.MoveRotation(rb.rotation * rotation);
+    }
+}
