a3d168bc319489838b28b4dd65f4af1c1286bf55
Shubham Tyagi <styagi.2301@gmail.com>
2018-09-15 13:54:27 +0530

    Implemented Tank health
    Created a canvas to display health slider underneath the tank...

---
+++ Assets/_MyStuff/Scripts/PlayerHealth.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public GameObject explosionParticlesPrefab;
+    public Slider slider; // required to change fill image value
+    public Image fillImage; // required to change color based on health
+    public int startingHealth = 100;
+    public Color zerohalthColor = Color.red;
+    public Color fullHealthColor = Color.green;
+
+    private ParticleSystem explosionParticles;
+    private AudioSource explosionAudio;
+    private int currentHealth;
+    private bool isDead;
+
+
+    private void Awake()
+    {
+        // instantiating particles in the beginning is the most efficient way to use particles
+        // then turn it on and off according to the situation.
+        explosionParticles = Instantiate(explosionParticlesPrefab).GetComponent<ParticleSystem>();
+        explosionAudio = explosionParticles.GetComponent<AudioSource>();
+        explosionParticles.gameObject.SetActive(false);
+    }
+
+    private void OnEnable()
+    {
+        isDead = false;
+        currentHealth = startingHealth;
+
+        SetHealthUI();
+    }
+
+    private void TakeDamage(int _damage)
+    {
+        // adjust the health, update the health UI, check if player is dead
+        currentHealth -= _damage;
+
+        SetHealthUI();
+
+        if (currentHealth <= 0 && !isDead)
+            OnDeath();
+    }
+
+    private void SetHealthUI()
+    {
+        // update slider value 
+        slider.value = currentHealth;
+
+        // update color value
+        fillImage.color = Color.Lerp(zerohalthColor, fullHealthColor, currentHealth / startingHealth);
+    }
+
+    private void OnDeath()
+    {
+        isDead = true;
+        // explosion particles position
+        explosionParticles.transform.position = transform.position;
+        explosionParticles.gameObject.SetActive(true);
+        explosionParticles.Play();
+        explosionAudio.Play();
+
+        // deactivate the tank
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/_MyStuff/Scripts/UIHealthRotationControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIHealthRotationControl : MonoBehaviour
+{
+    public bool freezeRotation;
+    private Quaternion originalRotation;
+
+    private void OnEnable()
+    {
+        freezeRotation = true;
+        originalRotation = transform.parent.localRotation; // canvas rotation
+    }
+
+    void Update()
+    {
+        // we dont want to change health slider/ canvas to chnage its rotation
+        // as the tank rotates.
+        // set rotation to original/default rotation in every frame.
+        if (freezeRotation)
+            transform.rotation = originalRotation;
+    }
+}
